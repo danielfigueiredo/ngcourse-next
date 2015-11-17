2f4dba109ab250d473c03c0cdcea3139d3c9cf8f
Igor Krivanov <igor.krivanov@rangle.io>
2015-11-17 09:18:25 -0500

    day 1 commit

--- app/src/app.ts
+++ app/src/app.ts
@@ -9,7 +9,27 @@ import '../css/styles.css';
 import * as angular from 'angular';
 import * as Rx from 'rx';
 
-angular.module('ngcourse', []);
+class MainDirectiveCtrl {
+  
+  userDisplayName: string;
+  
+  constructor() {
+    this.userDisplayName = 'Mike Tyson';
+  }
+  
+}
+
+angular.module('ngcourse', [])
+  .directive('ngcMain', () => {
+    return {
+      restrict: 'E',
+      scope: {},
+      template: '<span>Hello, {{ ctrl.userDisplayName }}.</span>',
+      controller: MainDirectiveCtrl,
+      controllerAs: 'ctrl',
+      bindToController: true
+    };
+  });
 
 angular.element(document).ready(
   () => angular.bootstrap(document, ['ngcourse'])
